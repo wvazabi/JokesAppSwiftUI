66e47bb2c3b61575a830b280aa83d4f0dc684324
Enes Kaya <eng.eneskaya@gmail.com>
2022-08-29 20:32:08 +0300

    Initial Commit

---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by Enes Kaya on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
